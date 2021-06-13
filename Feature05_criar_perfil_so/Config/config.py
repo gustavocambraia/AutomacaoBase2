3fb944d88834648e368bcc1a812e397b78803111
gustavocambraia <gustavocambraia@outlook.com>
2021-06-13 20:48:33 -0300

    config.py atualizados

--- Feature05_criar_perfil_so/Config/config.py
+++ Feature05_criar_perfil_so/Config/config.py
@@ -2,11 +2,11 @@ import ExcelUtils
 
 class DadosTest:
     #Web Drivers pastas
-    CHROMEDRIVER_PATH = "/tools/selenium/chromedriver"
-    GECKODRIVER_PATH = "/tools/selenium/geckodriver"
+    CHROMEDRIVER_PATH = r"C:\Program Files\JetBrains\PyCharm Community Edition 2020.1.2\bin\chromedriver.exe"
+    GECKODRIVER_PATH = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\geckodriver"
 
     #Data Driven Excel pasta
-    PATH = r"C:\Users\Lorenzo\Documents\Aulas Faculdade\ProjetoUC\Feature05_criar_perfil_so\PerfilSO.xlsx"
+    PATH = r"C:\Users\gusta\Desktop\AutomacaoBase2-master\AutomacaoBase2-master\Feature05_criar_perfil_so\PerfilSO.xlsx"
 
     #URL da pagina de login
     URL = "https://mantis.saojudas.base2.com.br/login_page.php"
@@ -32,7 +32,7 @@ class DadosTest:
                     lista.append(dado)
         return lista
 
-    LISTA_LOGIN = alimentar_list(PATH ,"Login")
+    LISTA_LOGIN = alimentar_list(PATH, "Login")
     LISTA_NOVO_PERFIL = alimentar_list(PATH, "NovoPerfilSO")
     LISTA_NOVO_PERFIL_INVALIDO = alimentar_list(PATH, "NovoPerfilSO_Invalido")
     LISTA_EDITAR_PERFIL = alimentar_list(PATH, "EditarPerfilSO")
