2e66de2bf69449081d8c1fe9ccecc7680379914b
gustavocambraia <gustavocambraia@outlook.com>
2021-06-13 21:00:44 -0300

    firefox adicionado no teste

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -5,7 +5,7 @@ from Config.config import TestData
 
 
 # @pytest.fixture(params=["chrome", "firefox"], scope='class')
-@pytest.fixture(params=["chrome"], scope='class')
+@pytest.fixture(params=["chrome", "firefox"], scope='class')
 def init_driver(request):
     if request.param == 'chrome':
         web_driver = webdriver.Chrome(executable_path=TestData.CHROME_EXECUTABLE_PATH)
